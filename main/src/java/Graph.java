625dc1a6d5ae65324851cac6315803a25bac5a3d
nakdolo <darhankusajn@gmail.com>
2023-02-18 21:11:09 +0600

    0.5

---
+++ main/src/java/Graph.java
@@ -0,0 +1,11 @@
+public class Graph {
+    String vertices;
+    int edge;
+    public Graph(String vertices, int edge) {
+        this.vertices = vertices;
+        this.edge = edge;
+    }
+    public void addEdge(int edge) {
+
+    }
+}
