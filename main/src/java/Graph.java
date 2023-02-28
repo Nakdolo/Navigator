dd5986be13e7cdcd3879d87a73bd2eedbc379f40
nakdolo <darhankusajn@gmail.com>
2023-02-28 16:34:10 +0600

    Update Graph.java

--- main/src/java/Graph.java
+++ main/src/java/Graph.java
@@ -1,11 +1,13 @@
 public class Graph {
     String vertices;
     int edge;
+    String[][] arr;
     public Graph(String vertices, int edge) {
         this.vertices = vertices;
         this.edge = edge;
+        this.arr = arr[][]
     }
-    public void addEdge(int edge) {
-
+    public void addEdge(String vertices) {
+        arr[][]
     }
 }
